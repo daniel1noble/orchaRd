37b206bea4b650f0f71658e5ae4985c9fc0fc339
Daniel Noble <daniel.wa.noble@gmail.com>
2021-04-17 12:24:17 +1000

    Added two function. Marginalised emana dn nre prediction interval calculation, added some examples to marginalised eman. TODO, connect with mod_results and orchard

--- R/mod_results.R
+++ R/mod_results.R
@@ -18,6 +18,25 @@ get_est <- function (model, mod) {
   return(table)
 }
 
+#' @title pred_interval_esmeans
+#' @description Function to get prediction intervals (crediblity intervals) from esmeans objects (metafor)
+#' @param model rma.mv object 
+#' @param esmeans result from emmeans::emmeans object
+#' @author Shinichi Nakagawa - s.nakagawa@unsw.edu.au
+#' @author Daniel Noble - daniel.noble@anu.edu.au
+#' @export
+pred_interval_esmeans <- function(model, esmeans){
+
+          results <- summary(esmeans)
+                 sigmas <- sum(model$sigma2)
+              test.stat <- qt(0.975, results$df)
+
+  results$lower.PI <- results$emmean - test.stat * sqrt(results$SE^2 + sigmas)
+  results$upper.PI <- results$emmean + test.stat * sqrt(results$SE^2 + sigmas)
+
+return(results)
+}
+
 
 #' @title get_pred
 #' @description Function to get prediction intervals (crediblity intervals) from rma objects (metafor)
@@ -34,6 +53,7 @@ get_pred <- function (model, mod) {
   
   if(len != 1){
   newdata <- matrix(NA, ncol = len, nrow = len)
+  
   for(i in 1:len) {
     # getting the position of unique case from X (design matrix)
     pos <- which(model$X[,i] == 1)[[1]]
@@ -51,6 +71,36 @@ get_pred <- function (model, mod) {
   table <- tibble::tibble(name = factor(name, levels = name, labels = name), lowerPR = lowerPR, upperPR = upperPR)
   return(table)
 }
+
+#' @title marginalised_means
+#' @description Function to to get marginalised means from met-regression models with single or multiple moderator variables that are both continuous or categorical.
+#' @param model rma.mv object 
+#' @param dat data frame used to fit rma.mv model
+#' @param pred predictor variable of interest that one wants marginalised means for. 
+#' @author Shinichi Nakagawa - s.nakagawa@unsw.edu.au
+#' @author Daniel Noble - daniel.noble@anu.edu.au
+#' @example \dontrun{
+#'warm_dat <- dat %>% filter(treat_type == "warm")
+#' model <- rma.mv(yi = lnrr, V = lnrr_vi, random = list(~1 | group_ID, ~1 | es_ID), mods = ~ experimental_design + trait.type + deg_dif + treat_end_days, method = "REML", test = "t", data = warm_dat,                               control=list(optimizer="optim", optmethod="Nelder-Mead")) 
+#'   overall <- marginalised_means(model, dat = warm_dat)
+#' across_trait <- marginalised_means(model, dat = warm_dat, pred = "trait.type")
+#' across_trait_by_degree_diff <- marginalised_means(model, dat = warm_dat, pred = "trait.type", at = list(deg_dif = c(5, 10, 15)), by = "deg_dif")
+#' across_trait_by_degree_diff_at_treat_end_days10 <- marginalised_means(model, dat = warm_dat, pred = "trait.type", at = list(deg_dif = c(5, 10, 15), treat_end_days = 10), by = "deg_dif")
+#' across_trait_by_degree_diff_at_treat_end_days10And50 <- marginalised_means(model, dat = warm_dat, pred = "trait.type", at = list(deg_dif = c(5, 10, 15), treat_end_days = c(10, 50)), by = "deg_dif")
+#' across_trait_by_treat_end_days10And50 <- marginalised_means(model, dat = warm_dat, pred = "trait.type", at = list(deg_dif = c(5, 10, 15), treat_end_days = c(10, 50)), by = "treat_end_days")
+#' across_trait_by_treat_end_days10And50_ordinaryMM <- marginalised_means(model, dat = warm_dat, pred = "trait.type", at = list(deg_dif = c(5, 10, 15), treat_end_days = c(10, 50)), by = "treat_end_days", weights = "prop")
+#' }
+#' @export
+#' 
+marginalised_means <- function(model, dat, pred, by = NULL, at = NULL, ...){
+  grid <- emmeans::qdrg(object = model, data = dat, at = at)
+    mm <- emmeans::emmeans(grid, specs = pred, df = model$df, by = by, ...)
+    mm_pi <- pred_interval(model, mm)
+
+  return(mm_pi)
+}
+
+
 
 #' @title firstup
 #' @description Uppercase moderator names
