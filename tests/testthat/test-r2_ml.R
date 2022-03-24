ea49317269253c3b1d95946ec3c294163fbd5784
Daniel Noble <daniel.noble@anu.edu.au>
2022-03-24 21:05:52 +1100

    Added some simple tests for R2

---
+++ tests/testthat/test-r2_ml.R
@@ -0,0 +1,24 @@
+context("Checking r2_ml function..")
+
+## English example
+data(english)
+
+# We need to calculate the effect sizes, in this case d
+english <- metafor::escalc(measure = "SMD", n1i = NStartControl, sd1i = SD_C, m1i = MeanC,
+                           n2i = NStartExpt, sd2i = SD_E, m2i = MeanE,
+                           var.names=c("SMD","vSMD"),
+                           data = english)
+
+english_MA <- metafor::rma.mv(yi = SMD, V = vSMD, random = list( ~ 1 | StudyNo, ~ 1 | EffectID), data = english)
+
+english_MA_R2 <- r2_ml(english_MA, data = english)
+
+
+testthat::test_that("Checking r2_ml function..", {
+  testthat::expect_equal(length(english_MA_R2), 2,
+                        info = "Checking r2 object has correct dimensions")
+
+  testthat::expect_equal(round(as.vector(english_MA_R2),2),
+                         round(c(0.00, 0.45 ), 2),
+                         info = "Checking R2 estimates are calculated correctly...")
+})
