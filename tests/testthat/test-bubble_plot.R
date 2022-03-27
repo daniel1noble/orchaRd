76727a95a9a362424167460d2094186d63ba8109
Daniel Noble <daniel.noble@anu.edu.au>
2022-03-27 15:36:42 +1100

    Fixed broken tests.

--- tests/testthat/test-bubble_plot.R
+++ tests/testthat/test-bubble_plot.R
@@ -3,12 +3,13 @@ context("Checking bubble_plot function...")
 data(lim)
 lim[, "year"] <- as.numeric(lim$year)
 lim$vi<- 1/(lim$N - 3)
-model<-rma.mv(yi=yi, V=vi, mods= ~Environment*year, random=list(~1|Article,~1|Datapoint), data=lim)
+model<-metafor::rma.mv(yi=yi, V=vi, mods= ~Environment*year,
+                       random=list(~1|Article,~1|Datapoint), data=na.omit(lim))
 
-test <- mod_results(model, mod = "year", group = "Article",
+test <- orchaRd::mod_results(model, mod = "year", group = "Article",
                     data = lim, weights = "prop", by = "Environment")
-plot1 <- bubble_plot(test, mod = "year", legend.pos = "top.left")
-plot2 <- bubble_plot(test, mod = "year", xlab = "Year", legend.pos = "top.left")
+plot1 <- orchaRd::bubble_plot(test, mod = "year", legend.pos = "top.left")
+plot2 <- orchaRd::bubble_plot(test, mod = "year", xlab = "Year", legend.pos = "top.left")
 
 
 testthat::test_that("Checking orchard_plot output ...", {
