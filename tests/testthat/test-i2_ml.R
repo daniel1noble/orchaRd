b8cc44e1f246beaf2f45d0071327d5ab44b083bd
Daniel Noble <daniel.noble@anu.edu.au>
2022-05-09 16:04:27 +1000

    Added a couple of other tests to check robust objects

--- tests/testthat/test-i2_ml.R
+++ tests/testthat/test-i2_ml.R
@@ -4,11 +4,17 @@ data(lim)
 lim$vi<-(1/sqrt(lim$N - 3))^2
 lim_MR<-metafor::rma.mv(yi=yi, V=vi, mods=~Phylum-1, random=list(~1|Article, ~1|Datapoint), data=lim)
 I2_lim_2 <- i2_ml(lim_MR, data=lim)
+lim_MR_robust <- metafor::robust(lim_MR, cluster=lim$Article)
+I2_lim_robust <- i2_ml(lim_MR_robust, data=lim)
 
 testthat::test_that("Checking i2 has correct dimensions..", {
   testthat::expect_equal(
     length(I2_lim_2), 3,
     info = "i2 has correct dimensions")
+
+  testthat::expect_equal(
+    length(I2_lim_robust), 3,
+    info = "i2 on robust object has correct dimensions")
 
 testthat::expect_equal(round(as.vector(I2_lim_2),2), round(c(84.32574, 48.19211, 36.13363), 2), info = "checking i2 estaimtes calculated correctly...")
 })
--- tests/testthat/test-r2_ml.R
+++ tests/testthat/test-r2_ml.R
@@ -13,12 +13,21 @@ english_MA <- metafor::rma.mv(yi = SMD, V = vSMD, random = list( ~ 1 | StudyNo, 
 
 english_MA_R2 <- r2_ml(english_MA, data = english)
 
+english_MA_robust <- robust(english_MA, cluster = english$StudyNo)
+
+english_MA_R2_robu <- r2_ml(english_MA_robust, data = english)
 
 testthat::test_that("Checking r2_ml function..", {
   testthat::expect_equal(length(english_MA_R2), 2,
                         info = "Checking r2 object has correct dimensions")
 
+  testthat::expect_equal(length(english_MA_R2_robu), 2,
+                         info = "Checking r2 robust object has correct dimensions")
+
   testthat::expect_equal(round(as.vector(english_MA_R2),2),
                          round(c(0.00, 0.45 ), 2),
                          info = "Checking R2 estimates are calculated correctly...")
+
+  testthat::expect_error(r2_ml(english_MA_robust, data = english, boot = 10),
+                         info = "Checking R2 estimates correctly throw errow...")
 })
