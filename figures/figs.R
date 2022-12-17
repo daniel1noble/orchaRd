66143706cbe614c13fc9467cddc3ed106c89a44a
Daniel Noble <daniel.noble@anu.edu.au>
2022-12-17 17:02:52 +1100

    Updated ignore file and cleaned figs script

--- figures/figs.R
+++ figures/figs.R
@@ -4,18 +4,13 @@
 ## Setup
 ###################
 	# Load packages
-
-	install.packages("pacman")
-pacman::p_load(devtools, tidyverse, metafor, patchwork, R.rsp, emmeans)
-
-devtools::install_github("daniel1noble/orchaRd", force = TRUE)
-library(orchaRd)
-
+		devtools::install_github("daniel1noble/orchaRd", force = TRUE)
 		library(orchaRd)
 		library(metafor)
 		library(tidyverse)
 		library(latex2exp)
 		library(patchwork)
+		library(flextable)
 
 	# Function for rounding  tables
 		round_df <- function(df, digits) {
@@ -45,45 +40,46 @@ library(orchaRd)
 		mod.habitat_hom
 
 ###################
-## Figure 2
+## Figure 1
 ###################
 
 	# Figure for het model
 	#dev.size() just find right sizing
-	orchard_plot(mod.habitat_het, data = pottier, group = "species_ID", mod = "habitat", xlab = "Developmental Acclimation Response Ratio (dARR)", angle = 45)
-	ggsave(filename = "./figures/fig2.pdf", width = 5.137255, height = 4.086274)
+	fig1a <-orchard_plot(mod.habitat_het, data = pottier, group = "species_ID", mod = "habitat", xlab = "Developmental Acclimation Response Ratio (dARR)", angle = 45)
+	#ggsave(filename = "./figures/fig1A.pdf", width = 5.137255, height = 4.086274)
 
 	# Homogen. Variance
-	orchard_plot(mod.habitat_hom, data = pottier, group = "species_ID", mod = "habitat", xlab = "Developmental Acclimation Response Ratio (dARR)", angle = 45)
-	ggsave(filename = "./figures/fig2B.pdf", width = 5.137255, height = 4.086274)
+	fig1b <- orchard_plot(mod.habitat_hom, data = pottier, group = "species_ID", mod = "habitat", xlab = "Developmental Acclimation Response Ratio (dARR)", angle = 45)
+	#ggsave(filename = "./figures/fig1B.pdf", width = 5.137255, height = 4.086274)
 
 	# Write the mod_table
 	form_tab_a <- round_df(mod_results(mod.habitat_hom, data = pottier, group = "species_ID", mod = "habitat")$mod_table, 2) 
 	form_tab_b <- round_df(mod_results(mod.habitat_het, data = pottier, group = "species_ID", mod = "habitat")$mod_table, 2) 
 
-	fig2_tab_a <- flextable::flextable(form_tab_a) %>% flextable::mk_par(part = "header", value = flextable::as_paragraph(c("Name", "Mean", "L 95% CI", "U 95% CI", "L 95% PI", "U 95% PI"))) %>% autofit()
-	save_as_image(fig2_tab_a, "./figures/fig2a_tab.png")
+	fig1_tab_a <- flextable::flextable(form_tab_a) %>% flextable::mk_par(part = "header", value = flextable::as_paragraph(c("Name", "Mean", "L 95% CI", "U 95% CI", "L 95% PI", "U 95% PI"))) %>% autofit()
+	#save_as_image(fig1_tab_a, "./figures/fig1a_tab.png")
 
-	fig2_tab_b <- flextable::flextable(form_tab_b) %>% flextable::mk_par(part = "header", value = flextable::as_paragraph(c("Name", "Mean", "L 95% CI", "U 95% CI", "L 95% PI", "U 95% PI"))) %>% autofit()
-	save_as_image(fig2_tab_b, "./figures/fig2b_tab.png")
+	fig1_tab_b <- flextable::flextable(form_tab_b) %>% flextable::mk_par(part = "header", value = flextable::as_paragraph(c("Name", "Mean", "L 95% CI", "U 95% CI", "L 95% PI", "U 95% PI"))) %>% autofit()
+	#save_as_image(fig1_tab_b, "./figures/fig1b_tab.png")
 
 ###################
-## Figure 3
+## Figure 2
 ###################
 	
 	 prop <- mod_results(mod.habitat_hom, data = pottier, group = "species_ID", weights = "prop")
 	equal <- mod_results(mod.habitat_hom, data = pottier, group = "species_ID", weights = "equal")
 
-	orchard_plot(mod.habitat_hom, data = pottier, group = "species_ID", xlab = "Developmental Acclimation Response Ratio (dARR)", angle = 45, weights = "prop") + ylim(c(-1,1)) + annotate("text", x = 1.2, y = 0, label = TeX(paste0("$\\mu$ = ", round(prop$mod_table[1,2], 2), ", 95% CI = ", round(prop$mod_table[1,3], 2), " to ", round(prop$mod_table[1,4], 2)))) + annotate("text", x = 1.5, y = 0, label = TeX("\\textbf{Proportional}"))
-	ggsave(filename = "./figures/fig3a.pdf", width = 4.462745, height = 3.560784)
+	fig2a <- orchard_plot(mod.habitat_hom, data = pottier, group = "species_ID", xlab = "Developmental Acclimation Response Ratio (dARR)", angle = 45, weights = "prop") + ylim(c(-1,1)) + annotate("text", x = 1.2, y = 0, label = TeX(paste0("$\\mu$ = ", round(prop$mod_table[1,2], 2), ", 95% CI = ", round(prop$mod_table[1,3], 2), " to ", round(prop$mod_table[1,4], 2)))) + annotate("text", x = 1.5, y = 0, label = TeX("\\textbf{Proportional}")) + theme(axis.text.y = element_blank())
 
 
-	orchard_plot(mod.habitat_hom, data = pottier, group = "species_ID", xlab = "Developmental Acclimation Response Ratio (dARR)", angle = 45, weights = "equal") + ylim(c(-1,1)) + annotate("text", x = 1.2, y = -0.1, label = TeX(paste0("$\\mu$ = ", round(equal$mod_table[1,2], 2), ", 95% CI = ", round(equal$mod_table[1,3], 2), " to ", round(equal$mod_table[1,4], 2)))) + annotate("text", x = 1.5, y = 0, label = TeX("\\textbf{Equal}"))
+	fig2b <- orchard_plot(mod.habitat_hom, data = pottier, group = "species_ID", xlab = "Developmental Acclimation Response Ratio (dARR)", angle = 45, weights = "equal") + ylim(c(-1,1)) + annotate("text", x = 1.2, y = -0.1, label = TeX(paste0("$\\mu$ = ", round(equal$mod_table[1,2], 2), ", 95% CI = ", round(equal$mod_table[1,3], 2), " to ", round(equal$mod_table[1,4], 2)))) + annotate("text", x = 1.5, y = 0, label = TeX("\\textbf{Equal}")) + theme(axis.text.y = element_blank())
 
-	ggsave(filename = "./figures/fig3b.pdf", width = 4.462745, height = 3.560784)
+
+	(fig2a | fig2b) + plot_annotation(tag_levels = "A", tag_suffix = ")") & theme(plot.tag = element_text(family = "Palatino", size = 24, face = "bold"))
+	ggsave(filename = "./figures/fig2.pdf", width = 8.839216, height = 3.929412)
 
 ###################
-## Figure 4
+## Figure 3
 ###################
 
 	# Load the dataset that comes with orchaRd
@@ -101,14 +97,17 @@ library(orchaRd)
 	                         data = warm_dat, 
 	                         control=list(optimizer="optim", optmethod="Nelder-Mead"))
 
-	orchaRd::orchard_plot(model_fish, group = "group_ID", mod = "trait.type", weights = "prop", data = warm_dat, xlab = "log Response Ratio (lnRR)", angle = 45, g = FALSE, legend.pos = "top.left", condition.lab = "Temperature Difference") + theme(legend.direction = "vertical", panel.grid = element_blank())
-	ggsave(filename = "./figures/fig4a.pdf", width = 4.337255, height = 4.258823)
+	fig3a <- orchaRd::orchard_plot(model_fish, group = "group_ID", mod = "trait.type", weights = "prop", data = warm_dat, xlab = "log Response Ratio (lnRR)", angle = 45, g = FALSE, legend.pos = "top.left", condition.lab = "Temperature Difference") + theme(legend.direction = "vertical", panel.grid = element_blank())
+	#ggsave(filename = "./figures/fig4a.pdf", width = 4.337255, height = 4.258823)
 	 
-	orchaRd::orchard_plot(model_fish, group = "group_ID", mod = "trait.type", at = list(deg_dif = c(5, 10, 15)), by = "deg_dif", weights = "prop", data = warm_dat, xlab = "log Response Ratio (lnRR)", angle = 45, g = FALSE, legend.pos = "top.left", condition.lab = "Temperature Difference") + theme(legend.direction = "vertical", panel.grid = element_blank())
-	ggsave(filename = "./figures/fig4b.pdf", width = 4.337255, height = 4.258823)
+	fig3b <- orchaRd::orchard_plot(model_fish, group = "group_ID", mod = "trait.type", at = list(deg_dif = c(5, 10, 15)), by = "deg_dif", weights = "prop", data = warm_dat, xlab = "log Response Ratio (lnRR)", angle = 45, g = FALSE, legend.pos = "top.left", condition.lab = "Temperature Difference") + theme(legend.direction = "vertical", panel.grid = element_blank(), axis.text.y = element_blank())
+	#ggsave(filename = "./figures/fig4b.pdf", width = 4.337255, height = 4.258823)
 
+	(fig3a | fig3b)  + plot_annotation(tag_levels = "A", tag_suffix = ")") & theme(plot.tag = element_text(family = "Palatino", size = 24, face = "bold"))
+	ggsave(filename = "./figures/fig3.pdf", width = 10.22745, height = 4.92549)
+ 
 ###################
-## Figure 5
+## Figure 4
 ###################
 	data(lim)
 	lim[, "year"] <- as.numeric(lim$year)
@@ -128,18 +127,18 @@ library(orchaRd)
 	                       random=list(~1|Article,~1|Datapoint), data=na.omit(lim))
 
 
-	fig5a <- orchaRd::orchard_plot(model2, group = "Article", mod = "rep_prop", legend.pos = "top.left", xlab = "Fisher's Z-transformed Correlation Coefficient (Zr)", data = lim2, angle = 45)
+	fig4a <- orchaRd::orchard_plot(model2, group = "Article", mod = "rep_prop", legend.pos = "top.left", xlab = "Fisher's Z-transformed Correlation Coefficient (Zr)", data = lim2, angle = 45) +  ggtitle('Categorical x Categorical') + theme(plot.title = element_text(hjust = 0.5))
 
 
 	lim_bubble <- orchaRd::mod_results(model, mod = "year", group = "Article",
 	                    data = lim, weights = "prop", by = "Environment")
 
-	fig5b <- orchaRd::bubble_plot(lim_bubble, data = lim, group = "Article", mod = "year", xlab = "Year", legend.pos = "top.left", ylab = "Fisher's Z-transformed Correlation Coefficient (Zr)")
+	fig4b <- orchaRd::bubble_plot(lim_bubble, data = lim, group = "Article", mod = "year", xlab = "Year", legend.pos = "top.left", ylab = "Fisher's Z-transformed Correlation Coefficient (Zr)") + ggtitle('Continuious x Categorical') + theme(plot.title = element_text(hjust = 0.5))
 
 	lim_bubble2 <- orchaRd::mod_results(model3, mod = "si", group = "Article", at=list(year = c(1972, 2012)), data = lim, weights = "prop", by = "year")
 
-	fig5c <- orchaRd::bubble_plot(lim_bubble2, data = lim, group = "Article", mod = "vi", legend.pos = "top.left", ylab = "Fisher's Z-transformed Correlation Coefficient (Zr)", xlab = "Sampling Standard Error", k = FALSE, g = FALSE) #+ coord_fixed(ratio = 0.2)
+	fig4c <- orchaRd::bubble_plot(lim_bubble2, data = lim, group = "Article", mod = "vi", legend.pos = "top.left", ylab = "Fisher's Z-transformed Correlation Coefficient (Zr)", xlab = "Sampling Standard Error", k = FALSE, g = FALSE) + ggtitle('Continuious x Continuious')+ theme(plot.title = element_text(hjust = 0.5))
 
 
-	(fig5a | fig5b | fig5c) + plot_layout(widths = unit(c(10,12,7), c("cm", "cm", "cm"))) + plot_annotation(tag_levels = "A", tag_suffix = ")") & theme(plot.tag = element_text(family = "Palatino", size = 24, face = "bold"))
-	ggsave(filename = "./figures/fig5.pdf", width = 15.968627, height = 6.509804)
+	(fig4a | fig4b | fig4c) + plot_layout(widths = unit(c(10,12,7), c("cm", "cm", "cm"))) + plot_annotation(tag_levels = "A", tag_suffix = ")") & theme(plot.tag = element_text(family = "Palatino", size = 24, face = "bold"))
+	ggsave(filename = "./figures/fig4.pdf", width = 15.968627, height = 6.509804)
