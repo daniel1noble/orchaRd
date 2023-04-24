c6c0399a300b983b8bc89c19a420d7ebc0975c13
Daniel Noble <daniel.noble@anu.edu.au>
2023-04-25 09:04:41 +1000

    Adding a few more test to make sure #22 is caught if there are ever problems

--- tests/testthat/test-i2_ml.R
+++ tests/testthat/test-i2_ml.R
@@ -3,8 +3,21 @@ context("Checking i2_ml function..")
 options(warn = -1)
 data(lim)
 lim$vi<-(1/sqrt(lim$N - 3))^2
+
+# Check two types of model specification. One with formula and one without
 lim_MR<-metafor::rma.mv(yi=yi, V=vi, mods=~Phylum-1, random=list(~1|Article, ~1|Datapoint), data=lim)
-I2_lim_2 <- orchaRd::i2_ml(lim_MR, data=lim)
+
+lim_MR2<-metafor::rma.mv(yi~1, V=vi, mods=~Phylum-1, random=list(~1|Article, ~1|Datapoint), data=lim)
+
+# Check point estimates
+ I2_lim_2 <- orchaRd::i2_ml(lim_MR, data=lim)
+I2_lim_22 <- orchaRd::i2_ml(lim_MR2, data=lim)
+
+# Checking boot
+ I2_lim_2_boot <- orchaRd::i2_ml(lim_MR, data=lim, boot =10)
+I2_lim_22_boot <- orchaRd::i2_ml(lim_MR2, data=lim, boot = 10)
+
+# Check it works with robust models
 lim_MR_robust <- metafor::robust(lim_MR, cluster=lim$Article)
 I2_lim_robust <- orchaRd::i2_ml(lim_MR_robust, data=lim)
 
@@ -12,6 +25,14 @@ testthat::test_that("Checking i2 has correct dimensions..", {
   testthat::expect_equal(
     length(I2_lim_2), 3,
     info = "i2 has correct dimensions")
+
+  testthat::expect_equal(
+    dim(I2_lim_2_boot)[1], 3,
+    info = "i2 boot has correct dimensions")
+
+  testthat::expect_equal(
+    dim(I2_lim_22_boot)[1], 3,
+    info = "i2 boot works with mod formula specification")
 
   testthat::expect_equal(
     length(I2_lim_robust), 3,
