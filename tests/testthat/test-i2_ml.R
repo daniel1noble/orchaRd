38e0678ef87321d26719b4ee03de5c554f8abcf3
Daniel Noble <daniel.noble@anu.edu.au>
2023-08-21 19:45:23 +1000

    Updating tests for i2. Updated documentation for package

--- tests/testthat/test-i2_ml.R
+++ tests/testthat/test-i2_ml.R
@@ -5,21 +5,21 @@ data(lim)
 lim$vi<-(1/sqrt(lim$N - 3))^2
 
 # Check two types of model specification. One with formula and one without
-lim_MR<-metafor::rma.mv(yi=yi, V=vi, mods=~Phylum-1, random=list(~1|Article, ~1|Datapoint), data=lim)
+lim_MR<-metafor::rma.mv(yi=yi, V=vi, mods=~Phylum-1, random=list(~1|Article, ~1|Datapoint), data = lim)
 
-lim_MR2<-metafor::rma.mv(yi~1, V=vi, mods=~Phylum-1, random=list(~1|Article, ~1|Datapoint), data=lim)
+lim_MR2<-metafor::rma.mv(yi~1, V=vi, mods=~Phylum-1, random=list(~1|Article, ~1|Datapoint), data = lim)
 
 # Check point estimates
- I2_lim_2 <- orchaRd::i2_ml(lim_MR, data=lim)
-I2_lim_22 <- orchaRd::i2_ml(lim_MR2, data=lim)
+ I2_lim_2 <- orchaRd::i2_ml(lim_MR)
+I2_lim_22 <- orchaRd::i2_ml(lim_MR2)
 
 # Checking boot
- I2_lim_2_boot <- orchaRd::i2_ml(lim_MR, data=lim, boot =10)
-I2_lim_22_boot <- orchaRd::i2_ml(lim_MR2, data=lim, boot = 10)
+ I2_lim_2_boot <- orchaRd::i2_ml(lim_MR,  boot =10)
+I2_lim_22_boot <- orchaRd::i2_ml(lim_MR2,  boot = 10)
 
 # Check it works with robust models
 lim_MR_robust <- metafor::robust(lim_MR, cluster=lim$Article)
-I2_lim_robust <- orchaRd::i2_ml(lim_MR_robust, data=lim)
+I2_lim_robust <- orchaRd::i2_ml(lim_MR_robust)
 
 testthat::test_that("Checking i2 has correct dimensions..", {
   testthat::expect_equal(
@@ -38,5 +38,5 @@ testthat::test_that("Checking i2 has correct dimensions..", {
     length(I2_lim_robust), 3,
     info = "i2 on robust object has correct dimensions")
 
-testthat::expect_equal(round(as.vector(I2_lim_2),2), round(c(84.32574, 48.19211, 36.13363), 2), info = "checking i2 estaimtes calculated correctly...")
+testthat::expect_equal(round(as.vector(I2_lim_2),2), round(c(84.32574, 48.19211, 36.13363), 2), info = "checking i2 estimates calculated correctly...")
 })
