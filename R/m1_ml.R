e37fcee22fcc86b14b0c06e6aa2e036205aebd94
Shinichi Nakagawa <itchyshin@gmail.com>
2024-04-01 07:10:23 +1100

    Update m1_ml.R

--- R/m1_ml.R
+++ R/m1_ml.R
@@ -111,7 +111,7 @@ m1_ml <- function(model,
 ml_m1 <- function(model){
 
   # total m1
-  M1_total <- sqrt(sum(model$sigma2)) / (abs(model$beta[[1]]) + sum(unlist(lapply(model$sigma2, sqrt))))
+  M1_total <- sum(unlist(lapply(model$sigma2, sqrt))) / (abs(model$beta[[1]]) + sum(unlist(lapply(model$sigma2, sqrt))))
   # m1 at different levels
   M1_each <-  sqrt(model$sigma2) / (abs(model$beta[[1]]) + sum(unlist(lapply(model$sigma2, sqrt))))
   names(M1_each) <- paste0("M1_", model$s.names)
